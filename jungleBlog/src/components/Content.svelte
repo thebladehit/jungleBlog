0477d160bd848a54539b3294187f458ee161c14e
Anton Dovzhenko <83024953+KobbAsa@users.noreply.github.com>
2023-11-14 22:55:35 +0200

    Added basic components (#6)
    * Initial setup
    closes #1
    
    * Add images for page
    closes #2
    
    * Deleted useless files and code lines
    
    * Added components directory and few components
    
    * Added header
    
    * Added footer
    
    * Added component for content
    
    * Added fonts to components
    
    ---------
    
    Co-authored-by: TheBladeHit <boqdanyarmolka@gmail.com>

---
+++ jungleBlog/src/components/Content.svelte
@@ -0,0 +1,10 @@
+<div class="content-wrapper">
+    Some text here
+</div>
+
+<style>
+    .content-wrapper {
+        flex: 1;
+        font-family: 'Inknut Antiqua', serif;
+    }
+</style>
---
+++ jungleBlog/src/components/Footer.svelte
@@ -0,0 +1,17 @@
+<footer>
+    © 2023 Kobbasa and TheBladeHit. All rights reserved
+</footer>
+
+<style>
+    footer {
+        background-color: #1a4814;
+        color: white;
+        padding: 1.3rem;
+        margin: 0;
+        font-family: 'Inknut Antiqua', serif;
+        font-size: 1.5rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
