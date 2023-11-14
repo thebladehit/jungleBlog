aeca40201b0ddbf41e21808f2b7eee09c083fd5b
Antonio <antony.dovzhenko@gmail.com>
2023-11-14 20:40:16 +0200

    Added footer

--- jungleBlog/src/App.svelte
+++ jungleBlog/src/App.svelte
@@ -1,8 +1,13 @@
 <script>
+
 import Header from "./components/Header.svelte";
+import Footer from "./components/Footer.svelte";
+
 </script>
 
 <Header/>
+Content here
+<Footer/>
 
 <style>
 </style>
---
+++ jungleBlog/src/components/Footer.svelte
@@ -0,0 +1,16 @@
+<footer>
+    © 2023 Kobbasa and TheBladeHit. All rights reserved
+</footer>
+
+<style>
+    footer {
+        background-color: #1a4814;
+        color: white;
+        padding: 1.3rem;
+        margin: 0;
+        font-size: 1.5rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
