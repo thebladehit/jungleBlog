c5d87ce2e41ac30386339a46d9361a5ac9ad654d
Antonio <antony.dovzhenko@gmail.com>
2023-11-15 14:19:12 +0200

    Added temporary array with cards props

---
+++ jungleBlog/src/articles.js
@@ -0,0 +1,38 @@
+export let articles = [
+    {
+        image: '/res/day1.jpg',
+        title: 'Day 1',
+        description: 'Today we are starting!',
+        text: 'This group of 27 men were supposed to find a possible route to build the Panama canal, and set off into the jungle with the hopes of discovering a pathway for it. The group knew they would be in the trees for potentially several weeks, but for some unknown reason they only brought enough food to last them for 10 days.\n' +
+            'It goes without saying that if you walk for 10 days into a jungle before you run out of food then you’re in trouble. The group started to break apart and some turned back, while others pressed forward. Members of the group began to drop one by one until only a handful were left. After starvation, disease and accidents only a fraction of the original 27 survived.',
+    },
+    {
+        image: '/res/day2.jpg',
+        title: 'Day 2',
+        description: 'Hunting!',
+        text: 'Towards the end of the second world war, a group of military personnel took a plane named the Gremlin special on a sight seeing flight over the Shangri-La Valley in New Guinea. During the pass through the valley the plane ran into thick mist and ended up crashing into the side of a hill, killing 19 out of the 24 people on board.',
+    },
+    {
+        image: '/res/day3.jpg',
+        title: 'Day 3', description: 'Fishing!',
+        text: 'A chances of a 5 year old surviving in the jungle alone is as close to impossible as you can get, but after seeing a group of monkeys in the trees, she began to follow them. She ate what they ate and drank from the same waters until eventually, the monkeys began to trust her and took her in as their own. She lived for several years with the monkeys and learnt how to survive as one of them.',
+    },
+    {
+        image: '/res/day4.jpg',
+        title: 'Day 4',
+        description: 'Building our camp!',
+        text: 'After getting lost with nothing more than a pair of leggings and a thin top, Shannon quickly found herself in a survival situation, facing starvation and trying to avoid crocodiles. She had to survive for 16 days with nothing more than she could find, and after walking through the thick vegetation she also had her clothes ripped to pieces and had to survive most of her ordeal naked.',
+    },
+    {
+        image: '/res/day5.jpg',
+        title: 'Day 5',
+        description: 'Fighting with bear!',
+        text: 'Even though this jungle isn’t one of the largest out there, moving through such thick vegetation can be extremely disorientating and slow progress. After realising that she had no idea which direction to walk in, she found an old abandoned boar den which she crawled inside to avoid the rain, sleeping there for several nights and hoping someone would find her.',
+    },
+    {
+        image: '/res/day6.jpg',
+        title: 'Day 6',
+        description: 'Chilling on the beach!',
+        text: 'Getting lost in such a huge wilderness area as the Gunung Mulu national park in Malaysia is never a good thing, but its even worse when you get yourself lost on purpose. Andrew Gaskell had his reasons for wanting to be alone, but this is no excuse to put your life in danger, and after coming close to death Andrew seemed to finally realise how stupid his decisions were.',
+    }
+];
