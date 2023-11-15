8f3bf360aaa25d9ee5287a4cfee45c6ebc1b78d5
Antonio <antony.dovzhenko@gmail.com>
2023-11-15 15:29:25 +0200

    Added card component and styles for it closes #7

---
+++ jungleBlog/src/components/Card.svelte
@@ -0,0 +1,78 @@
+<script>
+    export let article;
+</script>
+
+<div class="card">
+    <div class="text-content">
+        <h1>{article.title}</h1>
+        <p>{article.description}</p>
+    </div>
+    <div class="image-content">
+        <img src={article.image} alt={article.title} />
+    </div>
+</div>
+
+<style>
+    .card {
+        width: 400px;
+        height: 350px;
+        background-color: #c8eac1;
+        color: black;
+        overflow: hidden;
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+        transition: 0.3s;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        font-family: 'Inknut Antiqua', serif;
+    }
+
+    .card:hover {
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    }
+
+    .text-content {
+        padding: 0.5rem;
+        text-align: center;
+    }
+
+    .text-content h1 {
+        font-size: 1.5rem;
+        margin-bottom: 0.25rem;
+        position: relative;
+        padding-bottom: 0.5rem;
+        padding-left: 1rem;
+        text-align: left;
+    }
+
+    .text-content h1:after {
+        content: '';
+        position: absolute;
+        left: 1rem;
+        bottom: 0;
+        width: calc(100% - 2rem);
+        height: 3px;
+        background-color: #1a4814;
+        border-radius: 5px;
+    }
+
+    .image-content {
+        flex-grow: 1;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .image-content img {
+        max-width: 100%;
+        max-height: 200px;
+        object-fit: cover;
+    }
+
+    p {
+        font-size: 1rem;
+        line-height: 1.6;
+    }
+</style>
+
+
--- jungleBlog/src/components/Main.svelte
+++ jungleBlog/src/components/Main.svelte
@@ -1,16 +1,36 @@
 <script>
   import Content from "./Content.svelte";
+  import Card from "./Card.svelte";
+  import { articles } from "../articles.js";
 </script>
 
 <div class="content-wrapper">
   <Content />
 </div>
 
+<div class="cards-container">
+  {#each articles as article}
+    <Card {article} />
+  {/each}
+</div>
+
 <style>
   .content-wrapper {
     max-width: 1268px;
     margin: 0 auto;
-    flex: 1;
+    padding: 2rem;
     font-family: 'Inknut Antiqua', serif;
+  }
+
+  .cards-container {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    column-gap: 20px;
+    row-gap: 20px;
+    margin-top: 60px;
+    width: 1268px;
+    height: auto;
+    margin: 0 auto;
+    margin-bottom: 60px;
   }
 </style>
