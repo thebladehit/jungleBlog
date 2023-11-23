775d7a89051b9c4656db44448da7f10c0d8f9d48
Bogdan Yarmolka <65012653+thebaldehit@users.noreply.github.com>
2023-11-24 01:09:32 +0300

    Impemented static server (#27)
    closes #26

---
+++ backend/mimetypes.js
@@ -0,0 +1,13 @@
+const MIME_TYPES = {
+  html: 'text/html; charset=UTF-8',
+  js: 'application/javascript; charset=UTF-8',
+  json: 'application/json',
+  css: 'text/css',
+  png: 'image/png',
+  jpg: 'image/jpg',
+  ico: 'image/x-icon',
+  svg: 'image/svg+xml',
+  txt: 'text/plain',
+};
+
+module.exports = { MIME_TYPES };
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,34 @@
 
 require('dotenv').config();
 const http = require('node:http');
+const path = require('node:path');
+const { readFile, isFileExist } = require('./fs/fs.js');
+const { MIME_TYPES } = require('./mimetypes.js');
 
 const { PORT } = require('./config/config.js');
+const STATIC_PATH = path.resolve(__dirname, 'static');
 
-const server = http.createServer((req, res) => {
-  res.end('hello');
-}).listen(PORT, () => {
+const server = http.createServer(async (req, res) => {
+  const paths = [STATIC_PATH, req.url];
+  if (req.url === '/') paths.push('index.html');
+  const filePath = path.join(...paths);
+  if (!await isFileExist(filePath)) {
+    res.writeHead(404);
+    return void res.end('Not found');
+  }
+  try {
+    const data = await readFile(filePath);
+    const ext = path.extname(filePath).substring(1).toLowerCase();
+    const mimeType = MIME_TYPES[ext];
+    res.writeHead(200, { 'Content-Type': mimeType });
+    res.end(data);
+  } catch (err) {
+    res.writeHead(500);
+    res.end('Something went wrong');
+    // log it in file 
+  }
+});
+
+server.listen(PORT, () => {
   console.log(`Starting on ${PORT}...`)
 });
