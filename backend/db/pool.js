f176f371cdf225b6ca99d715437919545f9c005d
Bogdan Yarmolka <65012653+thebaldehit@users.noreply.github.com>
2023-11-27 22:56:22 +0300

    Feature/dbpool (#37)
    * Added .env.sample
    
    * Added pool of connections

---
+++ backend/db/pool.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+const { HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD } = require('../config/config.js');
+
+const pool = new Pool({
+  host: HOST,
+  port: DB_PORT,
+  database: DB_NAME,
+  user: DB_USER,
+  password: DB_PASSWORD
+});
+
+module.exports = { pool };
