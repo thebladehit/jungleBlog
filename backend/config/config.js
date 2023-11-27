3cf9ccb5c882323cc42dd51394f79adc73fb0d30
Bogdan Yarmolka <65012653+thebaldehit@users.noreply.github.com>
2023-11-27 17:41:02 +0300

    Added init database script (#35)
    closes #34

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,3 +1,8 @@
 const PORT = process.env.PORT ?? 3000;
+const HOST = process.env.HOST ?? '127.0.0.1';
+const DB_PORT = process.env.DB_PORT;
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
-module.exports = { PORT };
+module.exports = { PORT, HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD };
