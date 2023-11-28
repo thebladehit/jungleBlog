e5289c0a31ede2c41839ca46e3c39a8e304cb4dd
TheBladeHit <boqdanyarmolka@gmail.com>
2023-11-28 18:47:50 +0200

    Added getStory controller

--- backend/controllers/story.js
+++ backend/controllers/story.js
@@ -7,6 +7,7 @@ const getStories = async (req, res, logger) => {
     const data = await client.query('SELECT story_id, title, content FROM jungleBlog.stories');
     res.writeHead(200, { 'Content-Type': MIME_TYPES['json'] });
     res.end(JSON.stringify(data.rows));
+    client.release();
   } catch (err) {
     res.writeHead(500);
     res.end('Something went wrong');
@@ -14,8 +15,22 @@ const getStories = async (req, res, logger) => {
   }
 };
 
-const getStory = async (req, res) => {
-
+const getStory = async (req, res, logger) => {
+  try {
+    const splitedUrl = req.url.split('/');
+    const storyId = splitedUrl[splitedUrl.length - 1];
+    const parsedStoryId = +storyId;
+    if (isNaN(parsedStoryId)) throw new Error(`Invalid story id, id = "${storyId}"`);
+    const client = await pool.connect();
+    const data = await client.query(`SELECT story_id, title, content FROM jungleBlog.stories WHERE story_id=${storyId}`);
+    res.writeHead(200, { 'Content-Type': MIME_TYPES['json'] });
+    res.end(JSON.stringify(data.rows));
+    client.release();
+  } catch (err) {
+    res.writeHead(500);
+    res.end('Something went wrong');
+    await logger.log(err);
+  }
 };
 
 module.exports = { getStories, getStory };
