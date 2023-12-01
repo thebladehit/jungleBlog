38aad04a6864d1d80a5c6a7aeb0e167f4e9d4e0d
Antonio <antony.dovzhenko@gmail.com>
2023-12-01 23:50:00 +0200

    Added component for "About Us" page

---
+++ jungleBlog/src/components/AboutUs.svelte
@@ -0,0 +1,34 @@
+<div class="flex-container">
+    <div class = "text">
+        <h1>About My Blog</h1>
+        <p>Hello, my brave readers!</p>
+        <p>I'm a beginner in the world of survival, and this is my blog where I share my unique experience of surviving on a deserted island in the jungle.
+            Why am I here? I'm seeking adventure, testing myself, learning skills forgotten by the modern world, and of course, sharing it all with you.</p>
+        <p>Every day here is a new challenge. I write about how I build shelters from what I find in the jungle, how I find and purify water, gather food, or even try hunting.
+            I share my successes and failures, moments of joy and despair. But this blog isn't just about survival. It's also about connecting with nature, self-discovery, and
+            finding inner strength. I explore the environment, observe wildlife, try to understand the island's ecosystem, and my place in it. In this blog, you'll find not
+            only a description of my adventures but also photos, survival tips, information about the plants and animals I encounter, and much more. I want to share everything
+            I learn with you and possibly inspire someone for their own adventures.</p>
+        <p> So, if you're interested in nature, survival, or just want to feel part of something extraordinary, welcome to my blog. Join my adventure and discover the unknown
+            with me</p>
+    </div>
+</div>
+<style>
+    .flex-container {
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;
+        height: 85.9vh;
+    }
+
+    .text {
+        text-align: center;
+        width: 1268px;
+    }
+
+    @media screen and (max-width: 1280px) {
+        .text {
+            width: 90%;
+        }
+    }
+</style>
