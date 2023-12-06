cce33fdfd41f03099800273f8f6ccbaa2f5e57f2
Antonio <antony.dovzhenko@gmail.com>
2023-12-06 21:22:05 +0200

    Upgraded theme saving

--- jungleBlog/src/components/Header.svelte
+++ jungleBlog/src/components/Header.svelte
@@ -1,5 +1,6 @@
 <script>
     let isNavOpen = false;
+    const isDark = localStorage.getItem('theme')
 
     function toggleTheme(){
         const body = document.body;
@@ -20,13 +21,17 @@
         <span></span>
     </div>
     <nav class="{isNavOpen ? 'open' : ''}">
-        <a href="#articles">Articles</a>
+        <a href="/">Articles</a>
         <a href="/about">About</a>
         <a href="/feedback">Feedback</a>
         <a href="https://send.monobank.ua/jar/ATcjAVGpYV" target="_blank" rel="noreferrer noopener nofollow">Donate</a>
     </nav>
     <div class="theme-switcher">
-        <input on:click={toggleTheme} type="checkbox" id="theme-toggle"/>
+        {#if isDark === 'dark'}
+            <input on:click={toggleTheme} type="checkbox" id="theme-toggle" checked/>
+        {:else}
+            <input on:click={toggleTheme} type="checkbox" id="theme-toggle"/>
+        {/if}
         <label for="theme-toggle" class="toggle">
             <span class="toggle-handler"></span>
         </label>
