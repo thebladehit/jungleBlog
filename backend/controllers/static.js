4491f74f064e2f8133afa752d09dc98bdccec20e
Bogdan Yarmolka <65012653+thebaldehit@users.noreply.github.com>
2023-12-08 17:34:12 +0300

    Fixed bug when reload page on /article/:id (#58)
    * Fixed bug
    * Added response 304 code for static
    closes #44

--- backend/controllers/static.js
+++ backend/controllers/static.js
@@ -3,10 +3,18 @@ const { MIME_TYPES } = require('../mimetypes.js');
 const { readFile, isFileExist } = require('../fs/fs.js');
 
 const STATIC_PATH = path.resolve(__dirname, '..', 'static');
+let eTag = new Date().toISOString();
 
 const staticController = async (req, res, logger) => {
+  if (req.headers['if-none-match']) {
+    res.writeHead(304);
+    return void res.end();
+  }
   const paths = [STATIC_PATH, req.url];
-  if (req.url === '/') paths.push('index.html');
+  if (req.url === '/' || req.url.startsWith('/article')) {
+    paths.pop();
+    paths.push('/index.html');
+  } 
   const filePath = path.join(...paths);
   if (!await isFileExist(filePath)) {
     res.writeHead(404);
@@ -16,7 +24,7 @@ const staticController = async (req, res, logger) => {
     const data = await readFile(filePath);
     const ext = path.extname(filePath).substring(1).toLowerCase();
     const mimeType = MIME_TYPES[ext];
-    res.writeHead(200, { 'Content-Type': mimeType });
+    res.writeHead(200, { 'Content-Type': mimeType, 'ETag': eTag });
     res.end(data);
   } catch (err) {
     res.writeHead(500);
