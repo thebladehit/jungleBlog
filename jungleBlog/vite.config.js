efddc0cf1e4cf8bfe425ccf635e7343f6dbfb924
Antonio <antony.dovzhenko@gmail.com>
2023-12-12 17:45:15 +0200

    Updated vite config file

--- jungleBlog/vite.config.js
+++ jungleBlog/vite.config.js
@@ -4,4 +4,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
