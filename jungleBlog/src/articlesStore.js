2a8ea0aa9b5be9f36c90e8636ac4f24527f83f4c
Antonio <antony.dovzhenko@gmail.com>
2024-01-02 21:16:27 +0200

    Fixed fetching url

--- jungleBlog/src/articlesStore.js
+++ jungleBlog/src/articlesStore.js
@@ -3,7 +3,7 @@ import {writable} from "svelte/store";
 export const articlesData = writable([]);
 
 export async function fetchArticles(){
-    const url = "/api/story";
+    const url = "/story";
     const response = await fetch(url);
     if(!response.ok){
         throw new Error(`HTTP error! ${response.status}`)
