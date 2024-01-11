660dfed88ff1bcb06954d19eb27e8f59f87fa5dc
TheBladeHit <boqdanyarmolka@gmail.com>
2024-01-11 17:49:00 +0200

    Added store and utils

--- adminPanel/src/components/Comment.svelte
+++ adminPanel/src/components/Comment.svelte
@@ -1,19 +1,8 @@
 <script>
+  import { formatDateTime } from "../utils";
+
   export let commentData;
   export let sendDeleteComment;
-
-  const formatDateTime = (inputDate) => {
-    const date = new Date(inputDate);
-
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const year = String(date.getFullYear()).slice(-2);
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
-
-    return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
-  }
 
   const deleteComment = async () => {
     const response = await fetch('http://' + import.meta.env.VITE_HOST + '/comment', {
@@ -22,7 +11,6 @@
       body: JSON.stringify({ comment_id: commentData.comment_id })
     });
     if (!response.ok) {
-      console.log(response);
       throw new Error('Network error');
     }
     sendDeleteComment(commentData.story_id);
--- adminPanel/src/store.js
+++ adminPanel/src/store.js
@@ -1,9 +1,16 @@
 import { writable } from "svelte/store";
 
 export const commentsData = writable();
+export const storiesData = writable();
 
 export const fetchComments = async () => {
   const response = await fetch('http://' + import.meta.env.VITE_HOST + '/comment', { method: 'GET' });
+  if (!response.ok) throw new Error('Network error');
+  return await response.json();
+};
+
+export const fetchStories = async () => {
+  const response = await fetch('http://' + import.meta.env.VITE_HOST + '/story', { method: 'GET' });
   if (!response.ok) throw new Error('Network error');
   return await response.json();
 };
